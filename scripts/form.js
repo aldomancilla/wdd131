6a775064b1ebf73f70cecc3b1c7644cbbc290a36
aldomancilla <man24023@byui.edu>
2024-12-10 23:43:26 -0600

    finish

---
+++ scripts/form.js
@@ -0,0 +1,43 @@
+// Display the current year in the footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Display the last modified date of the document
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+// Array of product objects
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+// Populate the product
+const productSelect = document.getElementById("productName");
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+});
